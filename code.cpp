d99b5d54eba30f1c68b2bb88bd8aebbc1a281dab
creatnewsociety12 <64263489+creatnewsociety12@users.noreply.github.com>
2022-06-15 14:37:58 +0800

    Update code.cpp
    1、字符串转换
    2、把字符串里的数字提取出来，放到verctor里

--- code.cpp
+++ code.cpp
@@ -16,3 +16,45 @@ void bubble_sort(vector<int> &v)
     }
 }
 
+// 把char类型字符串转换为string类型
+string char_to_string(char *str)
+{
+    string s(str);
+    return s;
+}
+
+// 把string类型字符串转换为char类型
+char *string_to_char(string str)
+{
+    char *s = new char[str.size() + 1];
+    strcpy(s, str.c_str());
+    return s;
+}
+
+// 比较一个char类型字符串和string类型字符串是否相等
+bool char_string_equal(char *str1, string str2)
+{
+    string s1(str1);
+    return s1 == str2;
+}
+
+// 把一个char类型字符串里的数字提取出来，放到一个int类型vector里
+vector<int> extract_number(char *str)
+{
+    vector<int> v;
+    string s(str);
+    stringstream ss(s);
+    int i;
+    while (ss >> i)
+    {
+        v.push_back(i);
+        if (ss.peek() == ',')
+        {
+            ss.ignore();
+        }
+    }
+    return v;
+}
+
+
+
