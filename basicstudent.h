5f1f02f4b5f75cda39a20adb102e43754bab598e
creatnewsociety12 <64263489+creatnewsociety12@users.noreply.github.com>
2022-06-16 10:44:14 +0800

    头文件移动到文件夹里面

---
+++ basicstudent.h
@@ -0,0 +1,79 @@
+#include "headfile.h"
+
+// 定义一个学生数据结构体，包含学生的姓名、学号、成绩和年龄。
+struct student
+{
+    char name[20];
+    int number;
+    int score;
+    int age;
+    // 学生性别，0是女，1是男
+    int sex;
+    // 学生当前所在年级
+    int grade;
+    // 学生当前所在班级
+    int class_number;
+    // 学生当前所在班级的辅导员
+    char teacher[20];
+    // 对应的心理辅导员
+    char counselor[20];
+    // 所在寝室号码
+    char room_number[20];
+    // 对应校园卡号码
+    char card_number[20];
+    // 对应的学生类型，0是本科生，1是研究生
+    int type;
+    // 对应的学生所在学院类型
+    int college_type;
+    // 学生所学专业
+    char major[20];
+    // 学生所在学院
+    char college[20];
+    // 学生所在学校
+    char school[20];
+    // 学生所在学校的地址
+    char address[20];
+    // 学生所在学校的电话号码
+    char phone_number[20];
+    // 学生所在学校的邮箱
+    char email[20];
+    // 学生所在学校的网址
+    char website[20];
+    // 紧急联系人姓名
+    char emergency_name[20];
+    // 紧急联系人电话号码
+    char emergency_phone_number[20];
+    // 学生本学期是否已经缴费，0是未缴费，1是已经缴费
+    int is_pay;
+    // 学生本学期是否已经通过考试，0是未通过，1是已经通过
+    int is_pass;
+    // 学生是否存在历史未缴费记录，0是不存在，1是存在
+    int is_history_pay;
+    // 学生是否存在历史未通过考试记录，0是不存在，1是存在
+    int is_history_pass;
+    // 学生是否已经毕业，0是未毕业，1是已经毕业
+    int is_graduate;
+    // 学生是否已经退学，0是未退学，1是已经退学
+    int is_quit;
+    // 学生是否已经被删除，0是未被删除，1是已经被删除
+    int is_delete;
+    // 学生是否已经被禁用，0是未被禁用，1是已经被禁用
+    int is_disable;
+    // 学生是否已经被挂失，0是未被挂失，1是已经被挂失
+    int is_loss;
+    // 学生是否已经被锁定，0是未被锁定，1是已经被锁定
+    int is_lock;
+    // 学生是否已经被拉黑，0是未被拉黑，1是已经被拉黑
+    int is_black;
+    // 学生是否已经被激活，0是未被激活，1是已经被激活
+    int is_active;
+    // 学生是否已经被登记，0是未被登记，1是已经被登记
+    int is_register;
+    // 学生是否已经被绑定，0是未被绑定，1是已经被绑定
+    int is_bind;
+    // 学生是否已经被解绑，0是未被解绑，1是已经被解绑
+    int is_unbind;
+
+
+
+};
