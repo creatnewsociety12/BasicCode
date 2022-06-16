b0f2aed0537d01d6be5a39598f50a2deca103d7f
creatnewsociety12 <64263489+creatnewsociety12@users.noreply.github.com>
2022-06-16 10:42:02 +0800

    把学生结构体移动到一个单独头文件，因为需要经常添加字段

--- student.h
+++ student.h
@@ -1,51 +1,5 @@
 #include "headfile.h"
-
-// 定义一个学生数据结构体，包含学生的姓名、学号、成绩和年龄。
-struct student
-{
-    char name[20];
-    int number;
-    int score;
-    int age;
-    // 学生性别，0是女，1是男
-    int sex;
-    // 学生当前所在年级
-    int grade;
-    // 学生当前所在班级
-    int class_number;
-    // 学生当前所在班级的辅导员
-    char teacher[20];
-    // 对应的心理辅导员
-    char counselor[20];
-    // 所在寝室号码
-    char room_number[20];
-    // 对应校园卡号码
-    char card_number[20];
-    // 对应的学生类型，0是本科生，1是研究生
-    int type;
-    // 对应的学生所在学院类型
-    int college_type;
-    // 学生所学专业
-    char major[20];
-    // 学生所在学院
-    char college[20];
-    // 学生所在学校
-    char school[20];
-    // 学生所在学校的地址
-    char address[20];
-    // 学生所在学校的电话号码
-    char phone_number[20];
-    // 学生所在学校的邮箱
-    char email[20];
-    // 学生所在学校的网址
-    char website[20];
-    // 紧急联系人姓名
-    char emergency_name[20];
-    // 紧急联系人电话号码
-    char emergency_phone_number[20];
-
-
-};
+#include "basicstudent.h"
 
 // 定义一个学生类
 class CStudent
